7f0a6dd0d8927ec2272db9cb8766fa2c1f956f62
Lulseged Admasu <lulsegedgirma10@gmail.com>
2023-01-24 17:42:43 -0500

    done x

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,13 +1,3 @@
-// let mode_toggler = document.querySelector('.light-toggle')
-// window.addEventListener("resize", () => {
-//     if(window.matchMedia("(max-width: 768px)").matches) {
-//         mode_toggler.classList.add('collapse')
-//     }
-//     else {
-//         mode_toggler.classList.remove('collapse')
-//     }
-// });
-
 let navbar_items = document.querySelectorAll('.nav-link')
 navbar_items.forEach(link => {
     link.addEventListener('click', ()=> {
